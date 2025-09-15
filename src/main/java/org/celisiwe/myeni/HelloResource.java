c87cc4331f5a246a0bc66d1974c118c70ed19cf1
celisiwem <myeni.celisiwe@gmail.com>
2025-09-15 20:13:00 +0200

    created api endpoint class

---
+++ src/main/java/org/celisiwe/myeni/HelloResource.java
@@ -0,0 +1,16 @@
+package org.celisiwe.myeni;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/home")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
